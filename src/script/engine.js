e91ed3fb8a67f25d11b116ac1e8f1d28e90a36c6
DiogoVD <diogovdamascena@gmail.com>
2024-02-13 09:34:15 +0000

    continua implementação

--- src/script/engine.js
+++ src/script/engine.js
@@ -1,11 +1,12 @@
-const state={
+const state = {
     values:{
         n_players: document.getElementById("n_players"),
         n_palitos: document.getElementById("n_palitos")
     },
     telas: {
-        tela1: document.getElementsByClassName("config_container"),
-        tela2: document.getElementsById("palpites_cont") 
+        tela1: document.getElementById("config"),
+        tela2: document.getElementById("palp")
+        
     }
 }
 
@@ -13,15 +14,23 @@ let players = [];
 let palitos = [];
 let palpites = {};
 
+const nJogadores = 2;
+const n_palitos = 6;
+
 function config_porrinha(){
     
+    nJogadores = state.values.n_players.innerText;
+    state.telas.tela1.style.display = "none";
+    state.telas.tela2.style.display = "block";
+    console.log(nJogadores);
+
+    // players.push(prompt("informe o nome do jogador."));
+
 }
 
 function Init(){
-    
-    config_porrinha()
+    state.telas.tela2.style.display = "none";
 
 }
-
 
 Init()
