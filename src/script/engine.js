959f241b94fa9244593ef62e38d53b74a0be696d
DiogoVD <diogovdamascena@gmail.com>
2024-02-14 09:34:02 +0000

    codificando resultados

--- src/script/engine.js
+++ src/script/engine.js
@@ -8,39 +8,77 @@ const state = {
 
     telas: {
         tela1: document.getElementById("config"),
-        tela2: document.getElementById("palp")
+        tela2: document.getElementById("palp"),
+        telaRes: document.getElementById("res")
     },
 
     views: {
         lista: document.querySelector("#list_palp"),
+        lista_palp_result: document.querySelector("#list_result"),
         nomeJogador: document.querySelector("#player_name"),
+        resultado_btn: document.querySelector("#btn_result"),
+        proximo_btn: document.querySelector("#btn_next"),
+        vencedor_rodada: document.querySelector("#title")
     }
 }
 
 let players = [];
-let palitos = [];
+let palitos = {};
+let palitos_ver = [];
 let palpites = {};
+let palpites_ver =[];
 
 let nJogadores = 2;
 let nPalitos = 3;
 let contador = 0;
 
+let soma_dos_palitos = 0;
+
+
+
+function exibeResult(){
+    state.telas.tela2.style.display = "none";
+    state.telas.telaRes.style.display = "block";
+
+    soma_dos_palitos = palitos_ver.reduce((a,b) => a + b);
+    console.log(soma_dos_palitos);
+
+    state.views.lista_palp_result.innerHTML = "";
+    players.forEach((x) =>{
+        state.views.lista_palp_result.innerHTML += `|  ${x}  |  ${palpites[x]}  |  ${palitos[x]}  |<br>`
+        if(parseInt(palpites[x]) === soma_dos_palitos){
+            state.views.vencedor_rodada.innerHTML = x;
+        }
+    });
+
+
+}
+
+
 function capturaDados(){
-    palpites[players[contador]] = state.values.Jog_palpite.value;
-    palitos.push(state.values.jog_palitos.value);
+    if(palpites_ver.includes(state.values.Jog_palpite.value)===true){
+        alert("insira um novo palpite!!")
+        state.values.Jog_palpite.value = "";
 
-    state.values.Jog_palpite.value = "";
-    state.values.jog_palitos.value = "";
-
-    if(contador<(players.length -1)){
-        contador++;
-        state.views.nomeJogador.innerHTML = players[contador];
-        console.log(palpites);
     }else{
+        palpites[players[contador]] = state.values.Jog_palpite.value;
+        palitos[players[contador]] = state.values.jog_palitos.value;
+        palitos_ver.push(parseInt(state.values.jog_palitos.value));
+        palpites_ver.push(state.values.Jog_palpite.value)
 
+        state.values.Jog_palpite.value = "";
+        state.values.jog_palitos.value = "";
+
+        if(contador<(players.length -1)){
+            contador++;
+            state.views.nomeJogador.innerHTML = players[contador];
+        }else{
+            state.views.proximo_btn.style.display = "none";
+            state.views.resultado_btn.style.display = "block";
+
+        }
+        carregaNomesPalpites();
     }
-
-    carregaNomesPalpites();
     
 }
 
@@ -71,6 +109,7 @@ function config_porrinha(){
 
         state.telas.tela1.style.display = "none";
         state.telas.tela2.style.display = "block";
+        state.views.resultado_btn.style.display = "none";
         
         carregaNomesPalpites();
         state.views.nomeJogador.innerHTML = players[contador];
@@ -80,7 +119,17 @@ function config_porrinha(){
 
 function Init(){
     state.telas.tela2.style.display = "none";
+    state.telas.telaRes.style.display = "none";
 
 }
 
 Init()
+
+
+
+// regras
+
+// - Nao pode sugerir o mesmo palpites.
+// - apenas exibir o palpite apos escolha dos palitos.
+// - o ultimo bebe
+// - quem acerta sai da rodada
