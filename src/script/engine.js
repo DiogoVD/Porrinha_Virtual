9a558aa139291e838f084e8bda32ddaa5c7c3326
DiogoVD <diogovdamascena@gmail.com>
2024-02-16 00:46:38 +0000

    implementando tela de resultados

--- src/script/engine.js
+++ src/script/engine.js
@@ -18,22 +18,48 @@ const state = {
         nomeJogador: document.querySelector("#player_name"),
         resultado_btn: document.querySelector("#btn_result"),
         proximo_btn: document.querySelector("#btn_next"),
-        vencedor_rodada: document.querySelector("#title")
+        vencedor_rodada: document.querySelector("#title"),
+        set_palito: document.querySelector("#set_palito"),
+        set_palpite: document.querySelector("#set_palpite"),
+        palpite_btn: document.querySelector("#btn_palpite")
     }
 }
 
 let players = [];
-let palitos = {};
+let palitos = [];
 let palitos_ver = [];
-let palpites = {};
+let palpites = [];
 let palpites_ver =[];
 
 let nJogadores = 2;
 let nPalitos = 3;
 let contador = 0;
-
+let ganhadorRodada;
 let soma_dos_palitos = 0;
 
+
+
+function reiniciaRodada(){
+    let indice = players.indexOf(ganhadorRodada);
+
+    console.log(players);
+    console.log(palitos);
+    console.log(palitos_ver);
+    console.log(palpites);
+    console.log(palpites_ver);
+
+    players.splice(indice,1);
+    // palitos.splice(indice,1);
+    palitos_ver.splice(indice,1);
+    // palpites.splice(indice,1);
+    palpites_ver.splice(indice,1);
+
+    console.log(players);
+    console.log(palitos);
+    console.log(palitos_ver);
+    console.log(palpites);
+    console.log(palpites_ver);
+}
 
 
 function exibeResult(){
@@ -47,17 +73,17 @@ function exibeResult(){
     players.forEach((x) =>{
         state.views.lista_palp_result.innerHTML += `|  ${x}  |  ${palpites[x]}  |  ${palitos[x]}  |<br>`
         if(parseInt(palpites[x]) === soma_dos_palitos){
-            state.views.vencedor_rodada.innerHTML = x;
+            state.views.vencedor_rodada.innerHTML = `${x}<br>Ganhou a rodada`;
+            ganhadorRodada = x;
         }
     });
-
 
 }
 
 
 function capturaDados(){
     if(palpites_ver.includes(state.values.Jog_palpite.value)===true){
-        alert("insira um novo palpite!!")
+        alert("Palpite ja foi registrado. Informe um novo palpite!!")
         state.values.Jog_palpite.value = "";
 
     }else{
@@ -66,8 +92,7 @@ function capturaDados(){
         palitos_ver.push(parseInt(state.values.jog_palitos.value));
         palpites_ver.push(state.values.Jog_palpite.value)
 
-        state.values.Jog_palpite.value = "";
-        state.values.jog_palitos.value = "";
+        proximoJogador();
 
         if(contador<(players.length -1)){
             contador++;
@@ -75,32 +100,84 @@ function capturaDados(){
         }else{
             state.views.proximo_btn.style.display = "none";
             state.views.resultado_btn.style.display = "block";
+            state.views.palpite_btn.style.display = "none";
+            state.views.set_palito.style.display = "none";
+            state.views.nomeJogador.style.display = "none";
 
+            carregaNomesPalpites("sim");
         }
-        carregaNomesPalpites();
     }
     
 }
+function proximoJogador(){
+
+    if(state.values.Jog_palpite.value >=0 && state.values.Jog_palpite.value !==""){
+
+        state.values.Jog_palpite.value = "";
+        state.values.jog_palitos.value = "";
+    
+        state.views.set_palito.style.display = "block";
+        state.views.set_palpite.style.display = "none";
+        state.views.proximo_btn.style.display = "none";
+        state.views.palpite_btn.style.display = "block";
+        carregaNomesPalpites("nao");
+    
+    }else{
+        state.values.Jog_palpite.value = "";
+        alert(`Informe um número entre 0 e ${state.values.n_players.value * state.values.n_palitos.value}`);
+    }
+
+}
+
+function proximoPalpite(){
+
+    if(state.values.jog_palitos.value >=0 && state.values.jog_palitos.value <= state.values.n_palitos.value && state.values.jog_palitos.value !=="" ){
+
+        state.views.set_palito.style.display = "none";
+        state.views.set_palpite.style.display = "block";
+        state.views.proximo_btn.style.display = "block";
+        state.views.palpite_btn.style.display = "none";
+        carregaNomesPalpites("sim");
+
+    }else{
+        state.values.jog_palitos.value = "";
+        alert(`Informe um número entre 0 e ${state.values.n_palitos.value}`);
+    }
+
+}
 
 
-function carregaNomesPalpites(){
+function carregaNomesPalpites(monstraPalpite){
     state.views.lista.innerHTML = "";
-    players.forEach((x) =>{
-        state.views.lista.innerHTML += `| ${x} >> ${palpites[x]} << <br>`
-    });
+    if(monstraPalpite === "sim"){
+        players.forEach((x) =>{
+            state.views.lista.innerHTML += `| ${x} >> ${palpites[x]} << <br>`
+        });
+    }
+    if(monstraPalpite === "nao"){
+        players.forEach((x) =>{
+            state.views.lista.innerHTML += `| ${x} >> -- << <br>`
+        });
+    }
 }
 
 function capturaNomes(num_jogadores){
+    
     for(let i = 0; i<num_jogadores; i++){
-        players.push(prompt(`Informe o nome do jogador ${i+1}.`));
+        let nomePlayer = prompt(`Informe o nome do jogador ${i+1}.`)
+        if(nomePlayer === ""){
+            nomePlayer = `Jogador ${i+1}`
+        }
+        players.push(nomePlayer);
         palpites[players[i]] = 0;
     }
 }
 
 function config_porrinha(){
-    if(state.values.n_players.value === "" || state.values.n_palitos.value === "" ){
-        alert("Informe a quantidade de jogadores!!");
-        location.reload();
+    if( state.values.n_players.value === "" || state.values.n_palitos.value === "" || 
+        state.values.n_players.value < 2 || state.values.n_palitos.value < 3){
+            alert("Informe a quantidade de jogadores e palitos por jogador válidos!!");
+            location.reload();
     }else{
         nJogadores = state.values.n_players.value;
         nPalitos = state.values.n_palitos.value;
@@ -110,8 +187,11 @@ function config_porrinha(){
         state.telas.tela1.style.display = "none";
         state.telas.tela2.style.display = "block";
         state.views.resultado_btn.style.display = "none";
+        state.views.set_palpite.style.display = "none";
+        state.views.proximo_btn.style.display = "none";
+
         
-        carregaNomesPalpites();
+        carregaNomesPalpites("nao");
         state.views.nomeJogador.innerHTML = players[contador];
     }
     
@@ -120,7 +200,7 @@ function config_porrinha(){
 function Init(){
     state.telas.tela2.style.display = "none";
     state.telas.telaRes.style.display = "none";
-
+    state.views.resultado_btn.style.display = "none";
 }
 
 Init()
